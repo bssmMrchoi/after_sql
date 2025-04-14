a344fdad33069002f71e17fb90d169fe0228f215
김선예 <sistus0917@gmail.com>
2025-04-14 19:18:15 +0900

    수고하셨습니다:)

--- killing.sql
+++ killing.sql
@@ -65,3 +65,30 @@ where price >= 20000;
 select why
 from Customer
 where custid > 400;
+
+#살인 청부 가격이 20000원 이상인 타겟의 고객이름을 구하시오
+select *
+from Target t
+join Order_Kill ok on t.targetid = ok.targetid
+join Customer c on ok.custid = c.custid
+where t.price >= 20000;
+
+#1. target의 가격이 비싼 순으로 정렬하여 검색하세요.
+select *
+from Target
+order by price desc;
+#2. 날짜와 청부 이유를 내림차순으로 정렬한 결과를 출력하시오
+select ok.d_day, c.why
+from Order_Kill ok
+join Customer c on ok.custid = c.custid
+order by ok.d_day desc;
+#3. 살해방법을 날짜순(오름차순)으로 정렬한 결과를 출력하시오.
+select how
+from Order_Kill
+order by d_day asc;
+#4. 타겟을 이름순으로 검색하시오.
+select *
+from Target
+order by tname asc;
+
+
