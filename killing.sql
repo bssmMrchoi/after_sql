cba9167e422a67124c4ca5cab6ac23cf35735320
강준석 <kangjunsuk0503@gmail.com>
2025-04-14 20:06:25 +0900

    집계함수 문제해결

--- killing.sql
+++ killing.sql
@@ -56,13 +56,19 @@ select * from Customer;
 select * from Target;
 select * from Order_Kill;
 
-# orderby
+# 집계함수
 
-# 1. target의 가격이 비싼 순으로 정렬하여 검색하세요.
-select * from `Target` order by price desc;
-# 2. 날짜와 청부 이유를 내림차순으로 정렬한 결과를 출력하시오.
-select d_day, why from `Order_Kill` o join `Customer` c on o.custid = c.custid order by d_day desc;
-# 3. 살해방법을 날짜순(오름차순)으로 정렬한 결과를 출력하시오.
-select how from `Order_Kill` order by d_day;
-# 4. 타겟을 이름순으로 검색하시오.
-select tname from `Target` order by tname;
+# 1. 청구비용이 가장 비싼 것과 가장 싼 가격을 검색하시오.
+select max(price), min(price) from `Target`;
+# 2. 가장 큰 현상금을 가진 타겟의 고객의 이름과 이유를 구하시오
+select c.cname, c.why
+from Customer c join Order_Kill OK on c.custid = OK.custid
+    join Target T on OK.targetid = T.targetid
+where T.price = (select max(price) from Target);
+# 3. 고객번호가 471인 고객의 타겟의 가격 평균
+select avg(price)
+from `Target` t join `Order_Kill` ok on t.targetid = ok.targetid
+where ok.custid = 471;
+# 4. 청구비용이 가장 싼 타겟을 찾으시오.
+select * from `Target` where price = (select min(price) from Target);
+
