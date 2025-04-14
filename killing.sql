47d76cba1de141db45683408ed496e071ec3fa12
이효상 <nannnnsedaa@gmail.com>
2025-04-14 18:03:23 +0900

    1,3,4번 문재 해결

--- killing.sql
+++ killing.sql
@@ -57,4 +57,10 @@ select * from Customer;
 select * from Target;
 select * from Order_Kill;
 
+#1. 살해 방법이 ‘야스오’로 시작하는 행의 고객 id를 구하시오.
+select custid from Order_Kill where how like '야스오%';
+#3. 가격이 20000원 이상인 타겟의 이름을 구하시오.
+select tname from Target where price >= 20000;
+#4. 고객id가 400이 넘는 고객의 사유를 구하시오.
+select why from  Customer where custid > 400;
 
