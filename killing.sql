a92b7de2d22eef2891b4c1705708f76af2b6cc8e
a7a7-7 <hungmin090929@gmail.com>
2025-04-14 19:12:47 +0900

    Add ps query

--- killing.sql
+++ killing.sql
@@ -77,10 +77,3 @@ select * from Target order by price
 select d_day, why from Target T JOIN Order_Kill O ON T.targetid = O.targetid JOIN Customer C ON C.custid = O.custid order by d_day desc
 select d_day, how from Order_Kill order by d_day
 select tname from Target order by tname
-
---1. 청구비용이 가장 비싼 것과 가장 싼 가격을 검색하시오.
---2. 가장 큰 현상금을 가진 타겟의 고객의 이름과 이유를 구하시오
---3. 고객번호가 471인 고객의 타겟의 가격 평균
---4. 청구비용이 가장 싼 타겟을 찾으시오.
-select max(price) as 'max', min(price) as 'min' from Target;
-select avg(price) as 'Average of 471' from Target t on t.targetid = o.targetid join Order_Kill ok on ok.custid join Customer C on C.custid = 471;
