65dc85baaaf78373e9648c5afcf70baab640e3c4
bbanana502 <taehug662@gmail.com>
2025-04-21 19:48:41 +0900

    5 15번 문제

--- hospital.sql
+++ hospital.sql
@@ -68,6 +68,13 @@ having count(*) >= 1;
 4. 진료과별 평균 예약 수를 구하고, 예약이 1건 이상 있는 진료과만 출력하시오.
 
 5. 예약이 가장 적은 병원의 ID, 이름, 예약 건수를 출력하시오.
+select h.name, h.hospital_id, count(a.patient_id) as cnt
+from hospital h join appointment a
+on h.hospital_id=a.hospital_id
+group by h.hospital_id, h.name
+order by cnt asc limit 1;
+
+
 6. 30세 이상 환자들의 예약 건수를 병원별로 구하시오.
 select count(*), h.name
 from patient p join appointment a
@@ -107,21 +114,19 @@ group by h.name;
 
 
 13. ‘서울 중앙 병원’에 예약한 환자 목록을 예약일시 내림차순으로 출력하시오.
+
 14. 예약이 한 건도 없는 병원의 ID와 이름을 출력하시오.
 select h.hospital_id, h.name
 from hospital h left join appointment a
 on h.hospital_id=a.hospital_id
 where a.patient_id is Null;
 
-
-
-
-
-
-
-
-
-
-
-
 15. 병원별 예약된 고유 환자 수를 출력하시오. (같은 환자가 여러 번 예약했어도 1명으로 집계)
+select h.name, count(distinct p.name)
+from patient p join appointment a
+on p.patient_id=a.patient_id
+join hospital h on h.hospital_id=a.hospital_id
+group by h.name;
+
+
+
