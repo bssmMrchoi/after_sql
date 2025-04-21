0625558a673d9a30a43473dd5f699c0d55f90a1b
성민규 <qlalfql123@naver.com>
2025-04-21 17:41:57 +0900

    6문제

--- hospital.sql
+++ hospital.sql
@@ -49,11 +49,12 @@ INSERT INTO appointment (hospital_id, patient_id, reservation_datetime) VALUES
 select * from hospital;
 select * from patient;
 select * from appointment;
-#병원 이름과 진료과별로 예약 건수가 몇 건인지 구하고, 예약 건수가 1건 이상인 경우만 출력하되 예약 건수 기준으로 내림차순 정렬하시오.
+#1.병원 이름과 진료과별로 예약 건수가 몇 건인지 구하고, 예약 건수가 1건 이상인 경우만 출력하되 예약 건수 기준으로 내림차순 정렬하시오.
 select h.name,h.department,count(*) from hospital h join appointment a on h.hospital_id = a.hospital_id group by h.name,h.department
 having count(*) >=1 order by count(*)desc;
-#특정 날짜(예: 2025-04-22)에 예약한 환자 이름과 병원명, 예약 시각을 조회하시오.
-#30세 이상 환자들의 예약 건수를 병원별로 구하시오.
+#3.특정 날짜(예: 2025-04-22)에 예약한 환자 이름과 병원명, 예약 시각을 조회하시오.
+#6.30세 이상 환자들의 예약 건수를 병원별로 구하시오.
 select h.name,count(a.appointment_id) from hospital h join appointment a on h.hospital_id = a.hospital_id join patient p on a.patient_id = p.patient_id
 group by h.name, p.age having p.age >=30 order by h.name;
-
+#7.환자별 가장 최근 예약 정보를 출력하시오.
+select p.name,a.reservation_datetime from appointment a join patient p on a.patient_id = p.patient_id group by p.name,a.reservation_datetime order by reservation_datetime;
