426b42c00cff89484b47b92b48e69d85c3d5cb91
bssm <25_27@bssm.hs.kr>
2025-04-21 19:46:46 +0900

    제

--- hospital.sql
+++ hospital.sql
@@ -50,9 +50,8 @@ select * from hospital;
 select * from patient;
 select * from appointment;
 
-#병원 이름과 진료과별로 예약 건수가 몇 건인지 구하고, 예약 건수가 1건 이상인
+#1.병원 이름과 진료과별로 예약 건수가 몇 건인지 구하고, 예약 건수가 1건 이상인
 #경우만 출력하되 예약 건수 기준으로 내림차순 정렬하시오.
-#1
 select h.name, h.department, count(*)
 from hospital h join appointment a on h.hospital_id = a.hospital_id
 group by h.name, h.department;
@@ -60,8 +59,7 @@ having count(*) >= 1;
 order by count(*) desc;
 
 
-#30세 이상 환자들의 예약 건수를 병원별로 구하시오.
-#6
+#6. 30세 이상 환자들의 예약 건수를 병원별로 구하시오.
 select h.name, count(*)
 from hospital h join appointment a on p.hospital_id = a.hospital_id
 join patient p on a.patient_id = p.patient_id
@@ -69,35 +67,45 @@ where p.age >= 30
 group by h.name;
 
 
-# 환자별 가장 최근 예약 정보를 출력하시오.
-#7
+#7.환자별 가장 최근 예약 정보를 출력하시오.
 select p.name, max(a.reservation_datetime)
 from appointment a
 join patient p on a.patient_id = p.patient_id
 group by p.name;
 
 
-#예약 환자 중 ‘피부 트러블’을 증상으로 입력한 환자의 병원명과 예약일시를 출력하시오.
-#10
+#10.예약 환자 중 ‘피부 트러블’을 증상으로 입력한 환자의 병원명과 예약일시를 출력하시오.
 select h.name, a.reservation_datetime
 from hospital h join appointment a on h.hospital_id = a.hospital_id
 patient p join appointment a on p.patient_id = a.patient
 where p.symptoms = '피부 트러블';
 
 
-#각 병원별로 가장 나이가 많은 환자의 나이를 출력하시오.
-#12
+#12.각 병원별로 가장 나이가 많은 환자의 나이를 출력하시오.
 select h.name, max(p.age)
 from hospital h join appointment a on h.hospital_id = a.hospital_id
 join patient p on p.patient_id = a.patient_id
 group by h.name;
 
 
-#예약이 한 건도 없는 병원의 ID와 이름을 출력하시오.
-#14
+#14.예약이 한 건도 없는 병원의 ID와 이름을 출력하시오.
 select h.hospital_id, h.name
 from hospital h left join appointment a on h.hospital_id = a.hospital_id
 where a.hospital_id is null;
+
+
+#5.예약이 가장 적은 병원의 ID, 이름, 예약 건수를 출력하시오.
+select h.hospital_id, h.name, count(*) as cnt
+from hospital h join appointment a on h.hospital_id = a.hospital_id
+group by h.hospital_id, h.name
+order by cnt asc limit 1;
+
+
+
+#15. 병원별 예약된 고유 환자 수를 출력하시오. (같은 환자가 여러 번 예약했어도 1명으로 집계)
+select h.name, count(distinct a.patient_id)
+from  hospital h join appointment a on h.hospital_id = a.hospital_id
+group by h.name
 
 
 
