d339484348bf9209e310396f23ec269a43f50713
이효상 <nannnnsedaa@gmail.com>
2025-06-16 19:39:07 +0900

    서브쿼리, join

--- 통합 예제 문제2.sql
+++ 통합 예제 문제2.sql
@@ -32,3 +32,41 @@ select ENumber, count(*), avg(PCount) from tProduction where Pdate like '2020-03
 select ONumber, sum(RCount) from tReturn group by ONumber having sum(RCount) >= 500 order by sum(RCount) desc;
 # 16. 생산 건수가 많은 제품을 선별하고자 한다. 제품번호별 생산 건수를 구하되, 생산 건수가 3건 이상인 제품만 출력하시오. (제품번호 오름차순 정렬)
 select INumber, sum(PCount) from tProduction group by INumber having sum(PCount)>=3 order by INumber desc;
+
+## 서브쿼리
+
+# 1. 생산량이 가장 많은 제품의 제품코드와 생산량을 출력하라.(*단일행 서브쿼리*)
+select PNumber, PCount from tProduction where PCount = (select max(PCount) from tProduction);
+# 2. 평균 생산량보다 많은 생산 수량을 기록한 생산기록의 제품코드, 수량을 출력하라.(*스칼라 서브쿼리*)
+select INumber, PCount from tProduction where PCount > (select avg(PCount) from tProduction);
+# 3. 2020년 이후 입사한 직원 중 부서명이 ‘전자기기생산부’인 직원의 이름을 출력하라.(*IN + 서브쿼리*)
+select EName from tEmployee where StartDate >= '2020-01-01' and DNumber in (select DNumber from tDepartment where DName = '전자기기생산부');
+# 4. 최소 단가를 가진 제품의 이름과 단가를 출력하라.(*서브쿼리 활용*)
+select INumber, Price from tItem where Price = (select min(Price) from tItem);
+# 5. 반품 수량이 500 이상인 반품 건에 대해 해당 반품 건의 주문일자를 출력하라.(*반품 → 주문 → 날짜)*
+select RDate from tReturn where RCount >= 500;
+# 6. 생산기록이 없는 제품의 이름을 출력하라.(*NOT EXISTS 또는 `IN` + 서브쿼리*)
+select IName from tItem where not exists(select * from tProduction where tProduction.INumber = tItem.INumber);
+
+
+## join
+# 1. 직원 이름과 부서명을 함께 출력하되, 부서가 지정되지 않은 직원도 모두 포함하라. (*외부조인, NULL 포함*)
+select tEmployee.ENumber, tDepartment.DName from tEmployee left join tDepartment on tEmployee.DNumber = tDepartment.DNumber;
+# 2. 생산 테이블과 제품 테이블을 조인하여 생산일자와 함께 생산된 제품의 이름과 수량을 출력하라.
+select tProduction.PDate,tItem.IName, tProduction.PCount from tItem join tProduction on tItem.INumber = tProduction.INumber;
+# 3. 주문 테이블과 고객 테이블을 조인하여 고객의 이름과 각 주문일자를 출력하라.
+select tCustomer.CName, tOrder.ODate from tOrder join tCustomer on tOrder.CNumber = tCustomer.CNumber;
+# 4. 반품 테이블과 반품사유 테이블을 조인하여 반품번호, 반품일자, 반품사유를 출력하라.
+select tReturn.RNumber, tReturn.RDate, tReturnReason.RReason from tReturn join tReturnReason on tReturn.RRNumber = tReturn.RRNumber;
+# 5. 생산 테이블과 직원 테이블을 조인하여, 각 생산기록에 대해 직원명과 생산수량을 함께 출력하라.
+select tProduction.PNumber, tEmployee.ENumber, tProduction.PCount from tProduction join tEmployee on tProduction.ENumber = tEmployee.ENumber;
+# 6. 생산 테이블, 직원 테이블, 부서 테이블을 조인하여 각 생산기록에 대해 부서명, 직원명, 제품코드를 출력하라.
+select tDepartment.DName, tEmployee.EName, tProduction.INumber from tProduction join tEmployee on tProduction.ENumber = tEmployee.ENumber join tDepartment  on tEmployee.DNumber = tDepartment.DNumber;
+# 7. 고객이 주문한 제품명과 고객명을 출력하되, 제품명은 `tItem`에서, 고객명은 `tCustomer`에서 조회되도록 조인하라.
+select tItem.IName, tCustomer.CName from tItem join tProduction on tItem.INumber = tProduction.INumber join tOrder on tProduction.PNumber = tOrder.PNumber join tCustomer on tOrder.CNumber = tCustomer.CNumber;
+# 8. 반품이 발생한 주문 건에 대해 고객명, 제품명, 반품수량을 출력하라.
+select tCustomer.CName, tItem.IName, tReturn.RCount from tCustomer join tOrder on tCustomer.CNumber = tOrder.CNumber join tProduction on tOrder.PNumber = tProduction.PNumber join tItem on tProduction.INumber = tItem.INumber join tReturn on tOrder.ONumber = tReturn.ONumber;
+# 9. 직원이 소속된 부서명과 직급명을 각각 조인하여, 직원명과 함께 출력하라.(*2개의 테이블 조인*)
+select tDepartment.DName, tRank.RName, tEmployee.EName from tDepartment join tEmployee on tDepartment.DNumber = tEmployee.DNumber join tRank on tEmployee.RNumber = tRank.RNumber;
+# 10. 주문이 한 번도 없는 고객도 포함하여 고객명과 주문건수를 출력하라.(*LEFT JOIN + GROUP BY + COUNT*)
+select tCustomer.CName, count(*) from tCustomer left join tOrder on tCustomer.CNumber = tOrder.CNumber group by tCustomer.CName;
