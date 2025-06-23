7a663062dd68c16365a7e68352957bb6505ce67c
이호준 <hjsj23430@naver.com>
2025-06-23 17:15:23 +0900

    1211 이호준

--- shopping.sql
+++ shopping.sql
@@ -1,9 +1,11 @@
 use shopping
 
-create table hospital (
-    hospital_id INT PRIMARY KEY,   -- 병원 ID
-    name VARCHAR(100),             -- 병원명
-    department VARCHAR(100),       -- 진료과
-    director VARCHAR(100),         -- 병원장
-    capacity INT                   -- 층 수
-);'
+create table member_tbl_02 (
+   custno int(6) NOT NULL PRIMARY KEY,
+   custname varchar(20),
+   phone varchar(13),
+   address varchar(60),
+   joindate date,
+   grade char(1),
+   
+);
