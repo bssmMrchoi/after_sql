f4a8263db521f9ecf70cdec42f45285089bb59ba
이호준 <hjsj23430@naver.com>
2025-06-23 17:28:53 +0900

    1211 이호준

--- shopping.sql
+++ shopping.sql
@@ -9,3 +9,14 @@ create table member_tbl_02 (
    grade char(1),
    city char(1)
 );
+
+create table money_tbl_02 (
+   custno int(6),
+   salenol int(8) NOT NULL PRIMARY KEY,
+   pcost int(8),
+   amount int(4),
+   price int(8),
+   pcode varchar(4),
+   sdate date,
+    FOREIGN KEY (custno) REFERENCES member_tbl_02(custno)
+);
