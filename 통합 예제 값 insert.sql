bb5da55e93a3ba74bdcf369cab5984299dedac58
zuu3 <ojh27467242@gmail.com>
2025-06-24 11:50:19 +0900

    insert 수정

--- 통합 예제 값 insert.sql
+++ 통합 예제 값 insert.sql
@@ -319,4 +319,4 @@ insert into tReturn values('R4003','O4001',11,'RR0001','2022-01-08 16:09:20'); -
 insert into tReturn values('R4004','O4002',9,'RR0001','2022-01-13 13:25:53'); -- 전자기기
 insert into tReturn values('R5001','O5002',5,'RR0001','2022-01-07 10:53:05'); -- 음료
 insert into tReturn values('R5002','O5003',872,'RR0004','2022-01-20 10:53:05'); -- 음료
-insert into tReturn values('R5003','O5004',1212,'RR0002','2022-01-29 10:53:05'); -- 음료
+# insert into tReturn values('R5003','O5004',1212,'RR0002','2022-01-29 10:53:05'); -- 음료
